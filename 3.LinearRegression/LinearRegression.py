2a624e4ee0bd4ad8281a47ae10414194e29e22c7
NoNeil <xxm20080808@gmail.com>
2017-08-27 13:09:24 +0800

    add stochastic gradient descent

--- 3.LinearRegression/LinearRegression.py
+++ 3.LinearRegression/LinearRegression.py
@@ -2,59 +2,91 @@
 """
 Created on Wed Jun 22 20:47:39 2016
 
-@author: XuXiaoma
+@author: NoNeil
 """
 
 import numpy as np
+import math
+import sys
+
 
 class LinearRegression:
-    
+
     def __init__(self, X, y, tolerance=1e-5):
         """Initializes Class for Linear Regression
- 
+
         Parameters
         ----------
         X : ndarray(n-rows,m-features)
             Numerical training data.
- 
+
         y: ndarray(n-rows,)
             Interger training labels.
- 
+
         tolerance : float (default 1e-5)
             Stopping threshold difference in the loglikelihood between iterations.
- 
+
         """
-        self.m = X.shape[0]
-        self.n = X.shape[1]
+        self.n_samples = X.shape[0]
+        self.n_features = X.shape[1]
         self.tolerance = tolerance
         self.labels = y.reshape(y.size, 1)
-        #create weights equal to zero with an intercept coefficent at index 0
-        self.theta = np.zeros((self.n+1, 1))
-        #Add Intercept Data Point of 1 to each row
-        self.features = np.ones((self.m, self.n+1))
-        self.features[:, :self.n] = X
-        self.shuffled_features = self.features
-        self.shuffled_labels = self.labels
+        # create weights equal to zero with an intercept coefficent at index 0
+        self.theta = np.zeros((self.n_features + 1, 1))
+
+        # Add Intercept Data Point of 1 to each row
+        self.features = np.ones((self.n_samples, self.n_features + 1))
+        self.features[:, :self.n_features] = X
+
         self.loss_history = []
-        print "initialize"
-        
+        print("initialize")
+
+    # y = w * x
     def hypothesise(self, features):
         return np.dot(features, self.theta)
-        
-    def gradient(self):
-        return (1.0 / self.m) * np.dot(np.transpose(self.features), self.hypothesise(self.features) - self.labels)
-        
+
+    # batch gradient
+    # grad = (1 / n_samples) * x.T * (y' - w*x)
+    def batch_gradient(self):
+        return (1.0 / self.n_samples) * np.dot(np.transpose(self.features),
+                                               self.labels - self.hypothesise(self.features))
+
+    # loss = (1 / 2) * (y' - y)^2
     def loss_function(self):
-        return (0.5 / self.m) * np.sum(np.power(self.hypothesise(self.features) - self.labels, 2))
-        
-    def gradient_decent(self, alpha=1e-2, max_iter=1e3):        
+        return (0.5 / self.n_samples) * \
+            np.sum(np.power(self.hypothesise(self.features) - self.labels, 2))
+
+    # Batch Gradient Descent
+    def batch_gradient_decent(self, alpha=1e-2, max_iter=1e3):
         for i in range(int(max_iter)):
-            pre_loss = self.loss_function()            
-            grad = self.gradient()
-            self.theta = self.theta - alpha * grad
-            self.loss_history.append(pre_loss)
-            curr_loss = self.loss_function()
-            if(np.abs(pre_loss - curr_loss) < self.tolerance):
+            grad = self.batch_gradient()
+            self.theta += alpha * grad      # update theta
+
+            loss = self.loss_function()
+            if len(self.loss_history) > 0 and np.abs(
+                    self.loss_history[-1] - loss) < self.tolerance:
                 break
-            
-            
+            self.loss_history.append(loss)
+        pass
+
+    # stochastic gradient
+    # grad = x_i * (y' - w*x)
+    def stochastic_gradient(self, idx):
+        x_i = self.features[idx, :].reshape(1, self.n_features + 1)
+        y_i = self.labels[idx, :]
+        return np.dot(x_i.T, y_i - self.hypothesise(x_i))
+
+    # Stochastic Gradient Descent
+    def stochastic_gradient_descent(self, alpha=1e-2, max_iter=1e3):
+        for i in range(int(max_iter)):
+            # for each samples
+            for j in range(self.n_samples):
+                grad = self.stochastic_gradient(j)
+                self.theta += alpha * grad      # update theta
+
+            loss = self.loss_function()
+            if len(self.loss_history) > 0 and np.abs(
+                    self.loss_history[-1] - loss) < self.tolerance:
+                break
+            self.loss_history.append(loss)
+        pass
--- 3.LinearRegression/LinearRegressionImpl.py
+++ 3.LinearRegression/LinearRegressionImpl.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jun 22 20:50:14 2016
 
-@author: XuXiaoma
+@author: NoNeil
 """
 
 from LinearRegression import LinearRegression
@@ -10,34 +10,47 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def show_result(X, y, lr):
-    print "Result:"
+    print("Result:")
     X1 = np.linspace(0, 8, 1000)
     features = np.ones((X1.shape[0], 2))
     features[:, 0] = X1
     y1 = lr.hypothesise(features)
+    plt.interactive(False)
     plt.plot(X, y, 'bo')
     plt.plot(X1, y1, 'r-')
     plt.axis([0, 8, 0, 100])
-    plt.show()
-    
+    plt.show(block=True)
+
+
 def show_loss_history(loss_history):
-    print "History of the Cost:"
+    print("History of the Cost:")
     num_iters = len(loss_history)
     plt.plot(np.linspace(1, num_iters, num_iters), loss_history, 'b-')
     plt.axis([-20, num_iters, 0, np.max(loss_history)])
-    plt.show()
+    plt.show(block=True)
+
 
 # load data
 data = pd.read_csv("./lsd.dat", header=None, sep=r"\s+")
 data = data.as_matrix()
 X = data[:, 0: 1]
-y = data[:, 1: 2]
+y = data[:, -1]
 
-# invoke gradient decent
+# invoke batch gradient decent
 lr = LinearRegression(X, y, tolerance=1e-4)
-lr.gradient_decent(0.05, 1e5)
-print lr.theta
+lr.batch_gradient_decent(0.05, 1e5)
+print('theta of BGD: ', lr.theta.T)
+
+show_result(X, y, lr)
+show_loss_history(lr.loss_history)
+
+
+# invoke stochastic gradient decent
+lr = LinearRegression(X, y, tolerance=1e-4)
+lr.stochastic_gradient_descent(0.03, 1e3)
+print('theta of SGB: ', lr.theta.T)
 
 show_result(X, y, lr)
 show_loss_history(lr.loss_history)
