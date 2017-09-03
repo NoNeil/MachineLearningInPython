ae399aa6c7162dd8b7d796d0cf888d374e0c5f91
NoNeil <xxm20080808@gmail.com>
2017-09-03 14:41:05 +0800

    add Logistic Regression method

--- 3.LinearRegression/LinearRegressionImpl.py
+++ 3.LinearRegression/LinearRegressionImpl.py
@@ -5,10 +5,11 @@ Created on Wed Jun 22 20:50:14 2016
 @author: NoNeil
 """
 
-from LinearRegression import LinearRegression
+from LinearRegression import LinearRegression, LogisticRegression
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 def show_result(lr, title):
@@ -37,39 +38,39 @@ def show_result(lr, title):
     plt.show(block=True)
     plt.show()
 
-
+# Test for Linear Regression
 # load data
 data = pd.read_csv("./lsd.dat", header=None, sep=r"\s+")
 data = data.as_matrix()
-X = data[:, 0: 1]
+X = data[:, 0: -1]
 y = data[:, -1]
 
 
 # invoke batch gradient decent
 lr = LinearRegression(X, y, tolerance=1e-4)
 lr.batch_gradient_decent(0.05, 1e5)
-print('theta of BGD: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
+print('LinearRegression, theta of BGD: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
 show_result(lr, "Method: batch_gradient_decent")
 
 
 # invoke stochastic gradient decent
 lr = LinearRegression(X, y, tolerance=1e-4)
 lr.stochastic_gradient_descent(0.03, 1e3)
-print('theta of SGB: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
+print('LinearRegression, theta of SGB: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
 show_result(lr, "Method: stochastic_gradient_descent")
 
 
 # invoke general newton method
 lr = LinearRegression(X, y, tolerance=1e-4)
 lr.newton_general()
-print('theta of newton_general: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
+print('LinearRegression, theta of newton_general: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
 show_result(lr, "Method: newton_general")
 
 
 # invoke newton with Armijo search method
 lr = LinearRegression(X, y, tolerance=1e-4)
 lr.newton_armijo()
-print('theta of newton_armijo: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
+print('LinearRegression, theta of newton_armijo: ', lr.theta.T, ', num of iterations: ', len(lr.loss_history))
 show_result(lr, "Method: newton_armijo")
 
 # local weight linear regression
@@ -80,3 +81,26 @@ for tau in np.nditer(taus):
     plt.plot(lr.features[:, :-1], lr.labels, 'bo')
     plt.plot(X, y_estimate, 'r-')
     plt.show()
+
+# Test fot Logistic Regression
+# load data
+data = pd.read_csv("./apple_juice.dat", header=None, sep=r"\s+")
+data = data.as_matrix()
+X = data[:, 0: -1]
+y = data[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# create Logistic Regression model
+lr = LogisticRegression(X_train, y_train, tolerance=1e-5)
+lr.stochastic_gradient_descent(0.03, 1e3)
+print('Logistic Regression, theta of SGD: {0}, num of iterations: {1}'.format(lr.theta.T, len(lr.loss_history)))
+
+# predict
+X_test_bias = np.hstack((X_test, np.ones((X_test.shape[0], 1))))
+y_pred = lr.predict(X_test_bias)
+print('Accuracy rate: {:d}/{:d} = {:.6f}'
+      .format(np.sum(y_pred.T == y_test),
+              y_test.shape[0],
+              np.sum(y_pred.T == y_test).astype('float') / y_test.shape[0]))
+pass
+
